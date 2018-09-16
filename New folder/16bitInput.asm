af0560ce510568d9b3daf8eb2ce6f28e04fa94f7
Md. Abdul Lotif <cseku160212@gmail.com>
2018-09-16 17:43:16 +0600

    Update 16bitInput.asm

--- New folder/16bitInput.asm
+++ New folder/16bitInput.asm
@@ -56,7 +56,7 @@ SCAN PROC
     ;backup the digit to stack
     PUSH AX
     ;Multiply previous value by 10 and add new value
-    MOV AX, 10
+    MOV BX, 10
     MUL BX
     ;Pop new digit from stack
     POP BX
